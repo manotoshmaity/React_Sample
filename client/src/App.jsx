9f2ca6ac2d3376296ad276c0ce3c1cd22924a844
manotoshmaity <manotoshmaity7@gmail.com>
2026-02-10 11:29:34 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className="text-2xl font-bold text-blue-500">hello world</p>
+    </>
+  )
+}
+
+export default App
